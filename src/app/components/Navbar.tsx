fcb22e221200bd716d7cd804cd67147ed57b37b1
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2026-01-28 18:16:12 +0300

    feat: add global styles and layout for Next.js application
    - Created globals.css to import Tailwind CSS styles.
    - Implemented layout.tsx to define the root layout with Geist fonts.
    - Added page.tsx as the main entry point for the Home component.

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return(
+        <>
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+            <div className="flex items-center">
+        </div>
+    )
+}
